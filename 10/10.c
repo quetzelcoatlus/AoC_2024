4b933a66a8bb055ee26640b43911c9cb5654817d
quetzelcoatlus <hi@there.com>
2024-12-10 13:14:00 +0100

    Day 10
    What's funny is that part 2 was 'releasing' part 1 of constraint
    that allowed only one peak to be counted for each beginning of trail.

---
+++ 10/10.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BIG_N 1024
+
+char tab[BIG_N][BIG_N], bool[BIG_N][BIG_N];
+int n=-1,m;
+
+int find_trails(int i, int j){
+    if(tab[i][j] == '9')
+        if(!bool[i][j])
+            return bool[i][j]=1;
+
+    int sum=0;
+
+    for(int x=-1; x<=1; x++)
+        for(int y=-1; y<=1; y++)
+            if(abs(x+y)==1)
+                if(tab[i+x][j+y] == tab[i][j]+1 && i+x>=0 && j+y>=0 && i<n && j<m)
+                    sum += find_trails(i+x,j+y);
+
+    return sum;
+}
+
+int main(int argc, char* argv[]) {
+    char* name = argc > 1 ? argv[1] : "input";
+    FILE* f = fopen(name,"r");
+
+    int sum=0;
+
+    while(fscanf(f,"%s",(char *)&tab[++n]) > 0);
+
+    m=strlen(tab[0]);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(tab[i][j] == '0'){
+                memset(bool, 0, sizeof(bool));
+                sum += find_trails(i,j);
+            }
+        }
+    }
+
+    printf("%d\n",sum);
+
+    fclose(f);
+    return 0;
+}
