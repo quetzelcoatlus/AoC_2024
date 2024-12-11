dec1910f83a98e7b47dc7c8dadbefc6fcd8dff2b
quetzelcoatlus <hi@there.com>
2024-12-11 18:49:08 +0100

    Day 11 update
    Switching from 3d array to 2d to store information about already
    computed values. This reduces execution time by nearly 80-90% :O

--- 11/11b.c
+++ 11/11b.c
@@ -5,15 +5,15 @@
 #define BIG_N 500000
 #define BLINKS 75
 
-long int tab[BIG_N][BLINKS][2];
+long int tab[BIG_N][3];
 int n=0;
 
 long int count(long int value, int blinks){
     if(blinks == 0) return 1;
 
     for(int i=0;i<n;i++)
-        if(tab[i][blinks][0] == value)
-            return tab[i][blinks][1];
+        if(tab[i][0] == value && tab[i][1] == blinks)
+            return tab[i][2];
 
     long int result;
     if(value == 0){
@@ -29,8 +29,9 @@ long int count(long int value, int blinks){
         }
     }
 
-    tab[n][blinks][0]=value;
-    tab[n++][blinks][1]=result;
+    tab[n][0]=value;
+    tab[n][1]=blinks;
+    tab[n++][2]=result;
     return result;
 }
 
@@ -39,9 +40,8 @@ int main(int argc, char* argv[]) {
     long int sum=0, d;
 
     for(int i=0;i<BIG_N;i++)
-        for(int j=0;j<BLINKS;j++)
-            for(int k=0;k<2;k++)
-                tab[i][j][k] = -1;
+        for(int k=0;k<3;k++)
+            tab[i][k] = -1;
 
     char* name = argc > 1 ? argv[1] : "input";
     FILE* f = fopen(name,"r");
