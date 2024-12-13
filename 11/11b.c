8044d943652405c5b0608ae81243ea26186a8e39
quetzelcoatlus <hi@there.com>
2024-12-13 21:09:26 +0100

    Update Day 11: use hash map
    I was annoyed that part 2 of Day 11 takes 6 seconds (especially annoying
    during ./testall.sh scrip execution), so I decided to learn and switch
    to hash map from <search.h> standard library. Now it's executing in
    less than half second, which is acceptable. :)

--- 11/11b.c
+++ 11/11b.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <search.h>
+#include <string.h>
 
 #define BIG_N 500000
+#define STR_SIZE 32
 #define BLINKS 75
 
-long int tab[BIG_N][3];
+char keys[BIG_N][STR_SIZE];
+long int values[BIG_N];
 int n=0;
 
 long int count(long int value, int blinks){
     if(blinks == 0) return 1;
 
-    for(int i=0;i<n;i++)
-        if(tab[i][0] == value && tab[i][1] == blinks)
-            return tab[i][2];
+    char key[STR_SIZE];
+    sprintf(key,"%ld-%d",value,blinks);
+    ENTRY entry = {.key = key}, *found_entry;
+
+    if((found_entry=hsearch(entry, FIND)) != NULL)
+        return *( (long int *) found_entry->data);
 
     long int result;
     if(value == 0){
@@ -29,24 +36,27 @@ long int count(long int value, int blinks){
         }
     }
 
-    tab[n][0]=value;
-    tab[n][1]=blinks;
-    tab[n++][2]=result;
+    strcpy(keys[n], key);
+    values[n] = result;
+    entry.key = keys[n];
+    entry.data = &values[n];
+    hsearch(entry, ENTER);
+    n++;
+
     return result;
 }
 
 int main(int argc, char* argv[]) {
-
     long int sum=0, d;
 
-    for(int i=0;i<BIG_N;i++)
-        for(int k=0;k<3;k++)
-            tab[i][k] = -1;
+    hcreate(BIG_N);
 
     char* name = argc > 1 ? argv[1] : "input";
     FILE* f = fopen(name,"r");
     while(fscanf(f,"%ld",&d) > 0) sum+=count(d,BLINKS);
     fclose(f);
+
+    hdestroy();
 
     printf("%ld\n",sum);
 
